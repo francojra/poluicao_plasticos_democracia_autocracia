bee9f5d52e151dd6033fe8b59a9557fb0a76ac74
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-12-07 22:10:41 -0300

    Add files via upload

---
+++ poluicao_plasticos_democracia_autocracia.R
@@ -0,0 +1,14 @@
+
+# Poluição por plásticos em países democratas e autocratas ---------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 07/12/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/plastic-pollution ---------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### O primeito plástico sintético - Bakelite - foi produzido em 1907, marcando o início
+### da indústria de plástico global. Entretanto, o rápido crescimento na produção de plástico
+### global não foi realizado até os anos de 1950. Ao longo dos próximos 70 anos, a produção
+### anual de plásticos aumentou quase 230 vezes para 460 milhões de toneladas em 2019.
+
+
