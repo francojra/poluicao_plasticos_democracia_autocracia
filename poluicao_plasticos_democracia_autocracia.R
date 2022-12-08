59ccf202c8698d22cd1e3b4e4daf5cdbe138fccf
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-12-07 22:22:50 -0300

    Add files via upload

--- poluicao_plasticos_democracia_autocracia.R
+++ poluicao_plasticos_democracia_autocracia.R
@@ -11,4 +11,15 @@
 ### global não foi realizado até os anos de 1950. Ao longo dos próximos 70 anos, a produção
 ### anual de plásticos aumentou quase 230 vezes para 460 milhões de toneladas em 2019.
 
+### Poluição por plásticos está tendo um impacto negativo sobre o oceano e a saúde da fauna
+### selvagem.
+
+### Países de alta renda geram mais resíduos de plástico por pessoa.
+
+### A maior parte dos plásticos acabam em oceanos e vem de rios em países de média e baixa renda.
+### Isso ocorre devido a esses países tenderem a ter mal orientado destino do lixo, já os
+### países de alta renda apresentam o descarte correto do lixo.
+
+### Os sistemas de gestão de resíduos em todo o mundo são fundamentais para reduzir a 
+### poluição plástica.
 
